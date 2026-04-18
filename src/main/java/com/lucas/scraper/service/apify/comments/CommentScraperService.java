6484a5952b6a283ca6c1474c809e67102f27ecd6
Lucas Pimentel <lucaspimentel808@gmail.com>
2026-04-18 12:06:41 -0300

    Estrutura de service para outros scrapers
    getInstagramComments, getFacebookComments & getTiktokComments...

--- src/main/java/com/lucas/scraper/service/apify/comments/CommentScraperService.java
+++ src/main/java/com/lucas/scraper/service/apify/comments/CommentScraperService.java
@@ -1,10 +1,11 @@
 package com.lucas.scraper.service.apify.comments;
 
-import com.lucas.scraper.dto.in.FisherIn;
+import com.lucas.scraper.dto.in.FbCommentsIn;
 import com.lucas.scraper.dto.in.IgCommentsIn;
+import com.lucas.scraper.dto.in.TtCommentsIn;
+import com.lucas.scraper.dto.out.FbCommentsOut;
 import com.lucas.scraper.dto.out.IgCommentsOut;
-import com.lucas.scraper.dto.out.exception.GenericMessageOut;
-import com.lucas.scraper.exception.InvalidURLException;
+import com.lucas.scraper.dto.out.TtCommentsOut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -15,41 +16,56 @@ import java.util.List;
 public class CommentScraperService {
 
     private static final Logger log = LoggerFactory.getLogger(CommentScraperService.class);
-    private final IgCommentsGateway commentsGateway;
-    public CommentScraperService(IgCommentsGateway igCommentsGateway) {
-        this.commentsGateway = igCommentsGateway;
+    private final IgCommentsGateway igcommentsGateway;
+    private final FbCommentsGateway fbCommentsGateway;
+    private final TtCommentsGateway ttCommentsGateway;
+    public CommentScraperService(IgCommentsGateway igCommentsGateway,
+                                 FbCommentsGateway fbCommentsGateway,
+                                 TtCommentsGateway ttCommentsGateway) {
+        this.igcommentsGateway = igCommentsGateway;
+        this.fbCommentsGateway = fbCommentsGateway;
+        this.ttCommentsGateway = ttCommentsGateway;
     }
+
+
+    public List<IgCommentsOut> getInstagramComments(IgCommentsIn input){
+        return igcommentsGateway.getComments(input);
+    }
+
+    public List<TtCommentsOut> getTiktokComments(TtCommentsIn input){
+        return ttCommentsGateway.getComments(input);
+    }
+
+    public List<FbCommentsOut> getFacebookComments(FbCommentsIn input){
+        return fbCommentsGateway.getComments(input);
+    }
+
 
 
     // TODO: Verificar possibilidade de deixar filtros de comentários para a outra parte da aplicação
-    public List<IgCommentsOut> getComments(IgCommentsIn input){
-        return commentsGateway.getComments(input);
-    }
-
-
-    public List<IgCommentsOut> getInstagramFilteredComments(FisherIn input, String keyword){
-
-        if (!input.instagramUrl().contains("instagram")) {
-            log.warn("Comment Service: Requisição ignorada devido invalidade de URL");
-            throw new InvalidURLException("A URL deve conter o endereço de algum objeto do Instagram.");
-        }
-
-        log.info("Comment Service: Iniciando coleta em: {}", input.instagramUrl());
-
-        List<IgCommentsOut> raw = getComments(new IgCommentsIn(
-                List.of(input.instagramUrl()),
-                false,
-                false,
-                1000));
-
-         List<IgCommentsOut> response = raw
-                .stream()
-                .filter(item -> item.text()
-                        .toLowerCase()
-                        .contains(keyword.toLowerCase()))
-                .toList();
-
-         log.info("Instagram Comment Service: Foram filtradas {} itens com a keywords \"{}\" no Instagram", response.size(), keyword);
-         return response;
-    }
+//    public List<IgCommentsOut> getInstagramFilteredComments(FisherIn input, String keyword){
+//
+//        if (!input.instagramUrl().contains("instagram")) {
+//            log.warn("Comment Service: Requisição ignorada devido invalidade de URL");
+//            throw new InvalidURLException("A URL deve conter o endereço de algum objeto do Instagram.");
+//        }
+//
+//        log.info("Comment Service: Iniciando coleta em: {}", input.instagramUrl());
+//
+//        List<IgCommentsOut> raw = getInstagramComments(new IgCommentsIn(
+//                List.of(input.instagramUrl()),
+//                false,
+//                false,
+//                1000));
+//
+//         List<IgCommentsOut> response = raw
+//                .stream()
+//                .filter(item -> item.text()
+//                        .toLowerCase()
+//                        .contains(keyword.toLowerCase()))
+//                .toList();
+//
+//         log.info("Instagram Comment Service: Foram filtradas {} itens com a keywords \"{}\" no Instagram", response.size(), keyword);
+//         return response;
+//    }
 }
