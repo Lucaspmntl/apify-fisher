5a04485cb9bfab6c13d313ddfef7fc1ff5ac9929
Lucas Pimentel <lucaspimentel808@gmail.com>
2026-04-18 12:03:38 -0300

    Refatoração de polling
    Criação de classe única para este método, porque será usada em outras diferentes classes

--- src/main/java/com/lucas/scraper/service/apify/comments/IgCommentsGateway.java
+++ src/main/java/com/lucas/scraper/service/apify/comments/IgCommentsGateway.java
@@ -5,6 +5,7 @@ import com.lucas.scraper.dto.out.IgCommentsOut;
 import com.lucas.scraper.dto.out.startRun.RunResponseOut;
 import com.lucas.scraper.exception.PollingFailedException;
 import com.lucas.scraper.service.apify.ApifyGenericFeign;
+import com.lucas.scraper.service.apify.ApifyPolling;
 import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,8 +28,10 @@ public class IgCommentsGateway {
 
     public static final Logger log = LoggerFactory.getLogger(IgCommentsGateway.class);
     private final ApifyGenericFeign apifyClient;
-    public IgCommentsGateway(ApifyGenericFeign apifyClient) {
+    private final ApifyPolling polling;
+    public IgCommentsGateway(ApifyGenericFeign apifyClient, ApifyPolling polling) {
         this.apifyClient = apifyClient;
+        this.polling = polling;
     }
 
     public List<IgCommentsOut> getComments(IgCommentsIn input) {
@@ -37,8 +40,8 @@ public class IgCommentsGateway {
         log.info("Gateway: Iniciando a Run de id {}", run.data().runId());
 
 
-        if (!polling(run.data().runId()))
-            log.error("Gateway: Polling falhou.");
+        if (!polling.waitForSucceeded(run.data().runId()))
+            log.error("Gateway: Polling falhou para requisição de id {}.", run.data().runId());
 
         List<Map<String, Object>> rawComments = apifyClient.getDatasetItems(run.data().runId(), "Bearer " + token);
         log.info("Gateway: Run de id {} retornou {} itens", run.data().runId(), rawComments.size());
@@ -56,38 +59,5 @@ public class IgCommentsGateway {
                 //(List<repliesDTO>) item.get("replies")
         )).toList();
 
-    }
-
-    private boolean polling(String runId){
-        log.debug("Gateway: Iniciando polling para requisição de id: {}", runId);
-
-        String status;
-        int attempts = 1;
-        StopWatch watch = new StopWatch();
-        watch.start();
-        do{
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                watch.stop();
-
-                log.error("Erro na tentativa {} de polling: {}", attempts, e.toString());
-                throw new PollingFailedException("Erro interno na tentativa de polling.",
-                        500,
-                        watch.getTotalTimeSeconds(),
-                        attempts);
-            }
-
-            status = apifyClient.getRunDetails(runId, "Bearer " + token).data().status();
-
-            log.debug("Gateway: Tentativa {} de polling, status atual: {}", attempts, status);
-
-            // TODO: Tratar excessões como FAILED ou TIMED-OUT
-            // Verificar as possibilidades em: https://docs.apify.com/api/v2/actor-run-get
-
-            attempts++;
-        } while(!status.equalsIgnoreCase("SUCCEEDED"));
-
-        return true;
     }
 }
