8d119bcfa51bc19fcb8ae930cc3c57ff929b4ea5
Lucas Pimentel <lucaspimentel808@gmail.com>
2026-04-21 18:14:40 -0300

    DTO's de input para controllers

---
+++ src/main/java/com/lucas/scraper/dto/in/FbFisherCommentsIn.java
@@ -0,0 +1,12 @@
+package com.lucas.scraper.dto.in;
+
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
+public record FbFisherCommentsIn(
+
+        @URL
+        @NotBlank
+        String postUrl
+) {
+}
