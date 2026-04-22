80ce047ecd5be762a7c3a8c81276ec7ba573d8a3
Lucas Santos <Lucaspimentel808@gmail.com>
2026-04-22 17:19:16 -0300

    Commit before pull

--- src/main/java/com/lucas/scraper/dto/request/ApifyCommentsRequestDTO.java
+++ src/main/java/com/lucas/scraper/dto/request/ApifyCommentsRequestDTO.java
@@ -3,6 +3,8 @@ package com.lucas.scraper.dto.request;
 import java.util.List;
 
 public record ApifyCommentsRequestDTO(
+
+        @
         List<String> directUrls,
         boolean includeNestedComments,
         boolean includeReplies,
