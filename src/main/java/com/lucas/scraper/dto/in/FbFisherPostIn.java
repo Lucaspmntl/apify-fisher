0fba89c4304e59a609c8b0ae3ea9d7e32c108df1
Lucas Pimentel <lucaspimentel808@gmail.com>
2026-05-01 15:48:49 -0300

    Facebook post scraper
    Adição de dto's, método service, endpoint controller e gateway próprio

---
+++ src/main/java/com/lucas/scraper/dto/in/FbFisherPostIn.java
@@ -0,0 +1,11 @@
+package com.lucas.scraper.dto.in;
+
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
+public record FbFisherPostIn(
+        @URL
+        @NotBlank
+        String postUrl
+) {
+}
